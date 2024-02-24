4af100cee1ff4dec29ca26c1dc9b856116f700f9
MilthonB <milthonbor@gmail.com>
2024-02-23 17:23:42 -0700

    Imortaciones de modulos

---
+++ 02-bases/src/app.js
@@ -0,0 +1,7 @@
+
+const { emailTamplate } = require('./js-foundation/01-template')
+
+
+
+ 
+console.log(emailTamplate);
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,17 @@
+
+
+const emailTamplate = `
+    <div>
+        <h1> Hi, {{name}}</h1>
+        <p> Thanks you for you order</p>
+    </div>
+    `;
+    
+   
+
+    // modlulo de exportacion 
+    
+    module.exports = {
+        emailTamplate
+    }
+
