ae24b51931464706039681f3adf1744f07e0c2c0
MilthonB <milthonbor@gmail.com>
2024-02-24 18:24:20 -0700

    funcion factory

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,8 +1,21 @@
 
+const { getAge,getUuid } = require('./plugins')
+
 // const { emailTamplate } = require('./js-foundation/01-template')
 // require('./js-foundation/02-destructuring')
 // const { getuserById } = require('./js-foundation/03-callback')
-const { getuserById } = require('./js-foundation/05-factory')
+// const { getuserById } = require('./js-foundation/05-factory')
+const { buildMakePerson } = require('./js-foundation/05-factory')
+
+
+// Es un funcion que regresa una funcion 
+const makeAperson = buildMakePerson( { getAge,getUuid } )
+
+const obj = { name: 'Goku', birthdate: '1993-01-19'}
+
+const goku = makeAperson(obj)
+
+console.log(goku)
 
 
 
--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -4,26 +4,43 @@ const { getUuid,getAge } = require('../plugins/index')
 
 
 
-//crear fincion de flecha con desustructuracion de 
-//objeto al recibir los parametros
-const buildPerson = ( { name, birthdate } ) => {
+// Factory function es una funcion que regresa una funcion
+const buildMakePerson = ({ getUuid, getAge }) =>{
 
-    return {
-        id: getUuid(),
-        name,
-        birthdate,
-        age: getAge( birthdate ),
+    return  ({name,birthdate})=>{
+        return {
+            id: getUuid(),
+            name,
+            birthdate,
+            age: getAge( birthdate ),
+        }
     }
 }
 
-const obj = { name: 'Goku', birthdate: '1993-01-19'}
-
-const person = buildPerson(obj);
 
 
-console.log(person)
+//crear fincion de flecha con desustructuracion de 
+//objeto al recibir los parametros
+// const buildPerson = ( { name, birthdate } ) => {
+
+//     return {
+//         id: getUuid(),
+//         name,
+//         birthdate,
+//         age: getAge( birthdate ),
+//     }
+// }
+
+// const obj = { name: 'Goku', birthdate: '1993-01-19'}
+
+// const person = buildPerson(obj);
 
 
+// console.log(person)
+
+module.exports = {
+    buildMakePerson
+}
 
 
 
